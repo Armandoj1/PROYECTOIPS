11d63f2134aa307c9dab8bfe1f288ce5e8ec2602
Armandoj1 <jokecri88@hotmail.com>
2025-04-23 14:51:56 -0500

    push

--- IPSUPC/IPSUPC/obj/Debug/net8.0/IPSUPC.AssemblyInfo.cs
+++ IPSUPC/IPSUPC/obj/Debug/net8.0/IPSUPC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IPSUPC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f75a7b8a77c2a54ef25ab68af6f48a0fd31b034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ad8f515aa229220b8e2aaf67d532550fea3d4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("IPSUPC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IPSUPC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
