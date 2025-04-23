3714052db2c649c5f3b371712c728b8d9697c56a
Armandoj1 <jokecri88@hotmail.com>
2025-04-22 22:25:11 -0500

    aiuda

--- IPSUPC/IPSUPC/obj/Debug/net8.0/IPSUPC.AssemblyInfo.cs
+++ IPSUPC/IPSUPC/obj/Debug/net8.0/IPSUPC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IPSUPC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c71c1abca074c3f8ed7612036b02ecbcf87d81f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753a448c262411315913e61a5b1ce027fd62740f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IPSUPC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IPSUPC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
